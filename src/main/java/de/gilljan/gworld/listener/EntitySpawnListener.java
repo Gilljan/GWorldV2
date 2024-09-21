c9473afd9e1838de50521e314aa08958d20914db
Jannik Gillhaus <admin@gilljan.de>
2024-09-21 17:14:55 +0200

    EntitySpawnListener

--- src/main/java/de/gilljan/gworld/listener/EntitySpawnListener.java
+++ src/main/java/de/gilljan/gworld/listener/EntitySpawnListener.java
@@ -1,5 +1,9 @@
 package de.gilljan.gworld.listener;
 
+import de.gilljan.gworld.GWorld;
+import de.gilljan.gworld.data.properties.WorldProperty;
+import de.gilljan.gworld.data.world.WorldData;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -8,7 +12,38 @@ public class EntitySpawnListener implements Listener {
 
     @EventHandler
     public void onEntitySpawn(CreatureSpawnEvent event) {
-        //todo
+        if(event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CUSTOM) {
+            event.setCancelled(false);
+            return;
+        }
+
+        World world = event.getLocation().getWorld();
+
+        if(GWorld.getInstance().getWorldManager().getWorld(world.getName()) != null) {
+            event.setCancelled(false);
+            return;
+        }
+
+        WorldData worldData = GWorld.getInstance().getDataHandler().getWorld(world.getName());
+
+        if(WorldProperty.getValue(WorldProperty.ANIMAL_SPAWNING, worldData)) {
+            if(GWorld.ANIMALS.contains(event.getEntity().getClass().getSimpleName())) {
+                event.setCancelled(true);
+                return;
+            }
+        }
+
+        if(WorldProperty.getValue(WorldProperty.MONSTER_SPAWNING, worldData)) {
+            if(GWorld.MONSTER.contains(event.getEntity().getClass().getSimpleName())) {
+                event.setCancelled(true);
+                return;
+            }
+        }
+
+        if(WorldProperty.getValue(WorldProperty.DISABLED_ANIMALS, worldData).contains(event.getEntityType().name())
+                || WorldProperty.getValue(WorldProperty.DISABLED_MONSTERS, worldData).contains(event.getEntityType().name())) {
+            event.setCancelled(true);
+        }
     }
 
 }
