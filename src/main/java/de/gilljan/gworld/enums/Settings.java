d262c82474bf72f277e947ec3a70a60c0651dda6
Jannik Gillhaus <admin@gilljan.de>
2025-11-25 10:15:43 +0100

    Add method to map string settings to corresponding enum values

--- src/main/java/de/gilljan/gworld/enums/Settings.java
+++ src/main/java/de/gilljan/gworld/enums/Settings.java
@@ -33,4 +33,25 @@ public enum Settings {
         return values;
     }
 
+    public static Settings mapFromString(String setting) {
+        return switch (setting.toLowerCase()) {
+            case "weathercycle" -> WEATHER_CYCLE;
+            case "weather" -> WEATHER;
+            case "timecycle" -> TIME_CYCLE;
+            case "time" -> TIME;
+            case "difficulty" -> DIFFICULTY;
+            case "pvp" -> PVP;
+            case "monsterspawning" -> MONSTERS;
+            case "disabledmonsters" -> MONSTER_SPECIFIC;
+            case "animalspawning" -> ANIMALS;
+            case "disabledanimals" -> ANIMAL_SPECIFIC;
+            case "forcedgamemode" -> FORCED_GAMEMODE;
+            case "defaultgamemode" -> DEFAULT_GAMEMODE;
+            case "randomtickspeed" -> RANDOM_TICK_SPEED;
+            case "announceadvancements" -> ANNOUNCE_ADVANCEMENTS;
+            case "loadonstartup" -> LOAD_ON_STARTUP;
+            default -> null;
+        };
+    }
+
 }
