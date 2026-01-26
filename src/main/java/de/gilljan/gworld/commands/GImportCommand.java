a423ff203fa3b46c179bd9732794442e10ef4840
Jannik Gillhaus <admin@gilljan.de>
2026-01-26 14:00:06 +0100

    Refactor GImportCommand to unify player and console command handling

--- src/main/java/de/gilljan/gworld/commands/GImportCommand.java
+++ src/main/java/de/gilljan/gworld/commands/GImportCommand.java
@@ -8,10 +8,9 @@ import de.gilljan.gworld.enums.WorldTypeMapping;
 import de.gilljan.gworld.utils.SecureWorldNameUtil;
 import de.gilljan.gworld.utils.SendMessageUtil;
 import de.gilljan.gworld.world.ManageableWorld;
+import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-
-import java.util.List;
 
 public class GImportCommand extends ArgsCommand {
 
@@ -20,53 +19,58 @@ public class GImportCommand extends ArgsCommand {
     }
 
     @Override
-    public boolean executeCommandForPlayer(Player player, String[] args) { //todo check if world directory exists!
+    public boolean executeCommandForPlayer(Player player, String[] args) {
+        return handleImport(player, args);
+    }
+
+    @Override
+    public boolean executeConsoleCommand(ConsoleCommandSender console, String[] args) {
+        return handleImport(console, args);
+    }
+
+    private boolean handleImport(CommandSender sender, String[] args) {
         String worldName = args[0];
         String worldType = args[1];
         String generator = null;
 
         if (!SecureWorldNameUtil.isSecuredWorldName(worldName)) {
-            player.sendMessage(SendMessageUtil.sendMessage("SecurityMessage"));
+            sender.sendMessage(SendMessageUtil.sendMessage("SecurityMessage"));
             return false;
         }
 
-        if(GWorld.getInstance().getDataHandler().containsWorld(worldName)) {
-            player.sendMessage(SendMessageUtil.sendMessage("Import.failed").replace("%world%", worldName));
+        if (GWorld.getInstance().getDataHandler().containsWorld(worldName)) {
+            sender.sendMessage(SendMessageUtil.sendMessage("Import.failed").replace("%world%", worldName));
             return false;
         }
 
-        if(args.length == 3) {
+        if (args.length == 3) {
             generator = args[2];
         }
 
         WorldTypeMapping mapping = WorldTypeMapping.fromString(worldType);
 
-        if(mapping == null) {
-            player.sendMessage(SendMessageUtil.sendMessage("Import.failed"));
-            player.sendMessage(SendMessageUtil.sendMessage("Import.use"));
+        if (mapping == null) {
+            sender.sendMessage(SendMessageUtil.sendMessage("Import.failed"));
+            sender.sendMessage(SendMessageUtil.sendMessage("Import.use"));
             return false;
         }
 
-        WorldData worldData = new WorldData(worldName, mapping.getEnvironment(), mapping.getWorldType(),0, generator);
+        WorldData worldData = new WorldData(worldName, mapping.getEnvironment(), mapping.getWorldType(), 0, generator);
 
-        player.sendMessage(SendMessageUtil.sendMessage("Import.creating").replace("%world%", worldName));
+        sender.sendMessage(SendMessageUtil.sendMessage("Import.creating").replace("%world%", worldName));
 
         ManageableWorld mWorld = GWorld.getInstance().getWorldManager().addWorld(worldData);
 
-        if(!mWorld.importExisting()) {
-            player.sendMessage(SendMessageUtil.sendMessage("Import.failed").replace("%world%", worldName));
+        if (!mWorld.importExisting()) {
+            sender.sendMessage(SendMessageUtil.sendMessage("Import.failed").replace("%world%", worldName));
             return false;
         }
 
-        player.sendMessage(SendMessageUtil.sendMessage("Import.success").replace("%world%", worldName));
+        sender.sendMessage(SendMessageUtil.sendMessage("Import.success").replace("%world%", worldName));
 
         return true;
     }
 
-    @Override
-    public boolean executeConsoleCommand(ConsoleCommandSender console, String[] args) {
-        return false;
-    }
 
     @Override
     protected CompletionNode createCompletions() {
